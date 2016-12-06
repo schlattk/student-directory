57036cdc044ea5370a5548742009bc5c20baec2e
konradschlatte <konrad.schlatte@btinternet.com>
2016-12-06 20:53:22 +0000

    Exercises 1 & 2

---
+++ exercises/ex01.rb
@@ -0,0 +1,40 @@
+def input_students
+puts "Please enter the names of the student"
+puts "to finish, just hit return twice"
+#create an empty array
+students = []
+#get the first names
+name = gets.chomp
+#while the name is not empty repeat this code
+while !name.empty? do
+# add the student hash to the array
+students << {name: name, cohort: :november}
+puts "Now we have #{students.count} students"
+#get another name from the user
+name = gets.chomp
+end
+#return the array of students
+students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "------------"
+end
+
+def print(students)
+  students.each_with_index do |student,i|
+    if student[:name].downcase.index('a') == 0
+  puts "#{i+1}.#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+end
+
+def print_footer(students)
+puts "Overall, we have #{students.count} great students"
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
