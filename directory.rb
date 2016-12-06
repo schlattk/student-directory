94589eec45e924881a3673b40d75aee3a4017108
konradschlatte <konrad.schlatte@btinternet.com>
2016-12-06 17:10:32 +0000

    added print method

--- directory.rb
+++ directory.rb
@@ -11,13 +11,23 @@ students = [
 "Freddy Krueger",
 "The Joker",
 "Joffrey Baratheon",
-"Norman Bates"]
-#and then print them
-puts "The students of Villains Academy"
-puts "----------------"
-students.each do |student|
-  puts student
+"Norman Bates"
+]
+def print_header
+  puts "The students of Villains Academy"
+  puts "------------"
 end
 
-print "Overall, we have #{students.count} great students"
-#it's important that print() doesn't add new line characters
+def print(names)
+  names.each do |name|
+  puts name
+  end
+end
+
+def print_footer(names)
+puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
