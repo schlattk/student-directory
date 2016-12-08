5214c82259e681c63bd61c27019bb1b0e68966d9
konradschlatte <konrad.schlatte@btinternet.com>
2016-12-08 11:56:23 +0000

    final update

--- directory.rb
+++ directory.rb
@@ -47,7 +47,8 @@ name = STDIN.gets.chomp
 #while the name is not empty repeat this code
 while !name.empty? do
 # add the student hash to the array
-@students << {name: name, cohort: :november}
+update_students(name,"november")
+#@students << {name: name, cohort: :november}
 puts "Now we have #{@students.count} students"
 #get another name from the user
 name = STDIN.gets.chomp
@@ -66,11 +67,16 @@ def save_students
   file.close
 end
 
+def update_students(name,cohort)
+    @students << {name: name, cohort: cohort.to_sym}
+end
+
 def load_students(filename = "students.csv")
   file = File.open(filename, "r")
   file.readlines.each do |line|
   name, cohort = line.chomp.split(',')
-  @students << {name: name, cohort: cohort.to_sym}
+  update_students(name,cohort)
+  #@students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
