9cee22859246d37f3e8785ffb839291f8a566a83
konradschlatte <konrad.schlatte@btinternet.com>
2016-12-08 10:08:56 +0000

    updated to latest

--- directory.rb
+++ directory.rb
@@ -1,20 +1,66 @@
+@students = []
+
+def interactive_menu
+  loop do
+  print_menu
+  process(gets.chomp)
+  end
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "3"
+      save_students
+    when "9"
+    exit # this will cause the program to terminate
+    else
+    puts "I don't know what you meant, try again"
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit" # because we'll be adding more items
+end
+
+def show_students
+  # show the students
+    print_header
+    print_students_list
+    print_footer
+end
+
 def input_students
 puts "Please enter the names of the student"
 puts "to finish, just hit return twice"
-#create an empty array
-students = []
 #get the first names
 name = gets.chomp
 #while the name is not empty repeat this code
 while !name.empty? do
 # add the student hash to the array
-students << {name: name, cohort: :november}
-puts "Now we have #{students.count} students"
+@students << {name: name, cohort: :november}
+puts "Now we have #{@students.count} students"
 #get another name from the user
 name = gets.chomp
 end
-#return the array of students
-students
+end
+
+def save_students
+  #open the file for writing
+  file = File.open("students.csv", "w")
+  #iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name],student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 def print_header
@@ -22,17 +68,14 @@ def print_header
   puts "------------"
 end
 
-def print(students)
-  students.each do |student|
+def print_students_list
+  @students.each do |student|
   puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-puts "Overall, we have #{students.count} great students"
+def print_footer
+puts "Overall, we have #{@students.count} great students"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
